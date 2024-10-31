cdeeb350c5fdc7457071002995325cf0ef31a584
amithcabraal <147496214+amithcabraal@users.noreply.github.com>
2024-10-31 02:52:55 +0000

    Update genres.ts

--- src/data/genres.ts
+++ src/data/genres.ts
@@ -5,9 +5,14 @@ export const genres = [
     playlistId: '0SdFPQxsbteNQxx8pVoBiE'
   },
   {
-    id: '37i9dQZF1DX4o1oenSJRJd',
-    name: 'All Out 2000s',
-    playlistId: '37i9dQZF1DX4o1oenSJRJd'
+    id: '37i9dQZF1DWTJ7xPn4vNaz',
+    name: 'All Out 70s',
+    playlistId: '37i9dQZF1DWTJ7xPn4vNaz'
+  },
+  {
+    id: '37i9dQZF1DX4UtSsGT1Sbe',
+    name: 'All Out 80s',
+    playlistId: '37i9dQZF1DX4UtSsGT1Sbe'
   },
   {
     id: '37i9dQZF1DXbTxeAdrVG2l',
@@ -15,18 +20,13 @@ export const genres = [
     playlistId: '37i9dQZF1DXbTxeAdrVG2l'
   },
   {
-    id: '37i9dQZF1DWTJ7xPn4vNaz',
-    name: 'All Out 70s',
-    playlistId: '37i9dQZF1DWTJ7xPn4vNaz'
+    id: '37i9dQZF1DX4o1oenSJRJd',
+    name: 'All Out 2000s',
+    playlistId: '37i9dQZF1DX4o1oenSJRJd'
   },
   {
     id: '37i9dQZF1DX5Ejj0EkURtP',
     name: 'All Out 2010s',
     playlistId: '37i9dQZF1DX5Ejj0EkURtP'
-  },
-  {
-    id: '37i9dQZF1DX4UtSsGT1Sbe',
-    name: 'All Out 80s',
-    playlistId: '37i9dQZF1DX4UtSsGT1Sbe'
   }
 ];
