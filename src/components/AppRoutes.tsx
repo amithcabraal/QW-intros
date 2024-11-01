ead264a79dba7fe78e759031a52e9a62f3f6e1ed
amithcabraal <147496214+amithcabraal@users.noreply.github.com>
2024-11-01 21:31:57 +0000

    Update AppRoutes.tsx

--- src/components/AppRoutes.tsx
+++ src/components/AppRoutes.tsx
@@ -3,6 +3,9 @@ import { Routes, Route, Navigate, useSearchParams } from 'react-router-dom';
 import { Login } from './Login';
 import { SpotifyCallback } from './SpotifyCallback';
 import { GameRoom } from './GameRoom';
+import { HowToPlay } from './HowToPlay';
+import { Privacy } from './Privacy';
+import { Contact } from './Contact';
 
 export function AppRoutes() {
   const isAuthenticated = !!localStorage.getItem('spotify_token');
@@ -17,6 +20,9 @@ export function AppRoutes() {
       />
       <Route path="/login" element={<Login />} />
       <Route path="/callback" element={<SpotifyCallback />} />
+      <Route path="/how-to-play" element={<HowToPlay />} />
+      <Route path="/privacy" element={<Privacy />} />
+      <Route path="/contact" element={<Contact />} />
       <Route path="*" element={<Navigate to="/" />} />
     </Routes>
   );
